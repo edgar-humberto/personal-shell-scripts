9f1a4593a20b22d12a24ac9beb6f195c97c9eef3
Edgar Martinez <edgar-personal@edgars-personal.lan>
2020-01-11 12:48:59 -0800

    Fixed typeO in comment.

--- mkscript.sh
+++ mkscript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-# creates shell script tempalte file in the current working directory 
+# creates shell script template file in the current working directory 
 function mkscript {
     cat ~/scripts/_script_template > $PWD/$1
     chmod 755 $PWD/$1
