a5266a230164b97e52a696551f5b37414dc53a4f
Edgar Martinez <edgar-personal@edgars-personal.lan>
2020-01-11 13:35:32 -0800

    Moved the mkscript template to templates directory.

--- mkscript.sh
+++ mkscript.sh
@@ -2,6 +2,6 @@
 
 # creates shell script template file in the current working directory 
 function mkscript {
-    cat ~/scripts/_script_template > $PWD/$1
+    cat ~/scripts/templates/bash/_script_template > $PWD/$1
     chmod 755 $PWD/$1
 }
