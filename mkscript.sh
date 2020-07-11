a69ec8395a2e6bb8a0e8fd62f5394271ff29e494
le-garden-fox <le-garden-fox@example.com>
2020-07-11 09:26:49 -0700

    Add param number check on _mkscript

--- mkscript.sh
+++ mkscript.sh
@@ -2,6 +2,11 @@
 
 # creates shell script template file in the current working directory 
 function _mkscript {
+    if [ "$#" -ne 1 ]; then
+        echo "Invalid argument, need one argument, file_name"
+         exit 0
+    fi
+
     cat ~/scripts/templates/bash/_script_template > "$PWD/$1.sh"
     chmod 755 "$PWD/$1.sh"
 }
