0b3fa331391962987ac718bdd67b296a0032b4aa
le-garden-fox <le-garden-fox@example.com>
2020-08-09 18:11:35 -0700

    Add user to postgres script

--- postgres.sh
+++ postgres.sh
@@ -1,13 +1,12 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 # this is a demo postgres server for when I need one
-function _run_postgres {
+function _run_postgres() {
     if [ ! -d $HOME/docker-volumes/postgres ]; then
-        mkdir -p $HOME/docker-volumes/postgres 
+        mkdir -p $HOME/docker-volumes/postgres
     fi
     if [ ! "$(docker ps -a | grep postgres)" ]; then
-        docker run -d -p 5432:5432 --name="postgres" --env POSTGRES_PASSWORD=example -v $HOME/docker-volumes/postgres:/var/lib/postgresql/data postgres:11.8
+        docker run -d -p 5432:5432 --name="postgres" --env POSTGRES_USER=postgres --env POSTGRES_PASSWORD=example -v $HOME/docker-volumes/postgres:/var/lib/postgresql/data postgres:11.8
     fi
 
 }
-
